1274efab2cbc1235c37446b8d7de6ce87b49428e
amelazzi <ea_azzi@esi.dz>
2020-03-04 15:33:22 +0100

    update: edit Profile xml

--- app/src/main/java/com/example/iwatch/Activities/Profile.kt
+++ app/src/main/java/com/example/iwatch/Activities/Profile.kt
@@ -1,13 +1,26 @@
 package com.example.iwatch.Activities
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.iwatch.Adapters.GenreAdapter
 import com.example.iwatch.R
+import kotlinx.android.synthetic.main.activity_profile.*
+
 
 class Profile : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        genre_list.adapter = GenreAdapter(this)
+
+        btn_edit_profile.setOnClickListener {
+            val editProfile = Intent(this, EditProfile::class.java)
+            startActivity(editProfile)
+            System.out.println("hello")
+        }
     }
 }
