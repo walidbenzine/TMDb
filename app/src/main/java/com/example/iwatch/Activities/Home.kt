9a5049a6b5f773a1f76e9a2ef8f050cbef3041dc
SADIA YEBDA <es_yebda@esi.dz>
2020-03-07 22:58:12 +0100

    feat: add Home Fragment

--- app/src/main/java/com/example/iwatch/Activities/Home.kt
+++ app/src/main/java/com/example/iwatch/Activities/Home.kt
@@ -6,8 +6,11 @@ import com.example.iwatch.R
 
 class Home : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView( R.layout.activity_home)
     }
+
+
 }
