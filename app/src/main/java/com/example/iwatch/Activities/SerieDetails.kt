076db7473f8320cb877858ed7b3f78a4997e9abb
amelazzi <ea_azzi@esi.dz>
2020-04-02 18:09:39 +0200

    feat: attribute fragments to serie detail tab

--- app/src/main/java/com/example/iwatch/Activities/SerieDetails.kt
+++ app/src/main/java/com/example/iwatch/Activities/SerieDetails.kt
@@ -1,13 +1,62 @@
 package com.example.iwatch.Activities
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.iwatch.Fragments.AssociatedSeriesFragment
+import com.example.iwatch.Fragments.CommentsFragment
+import com.example.iwatch.Fragments.SeasonFragment
 import com.example.iwatch.R
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_serie_details.*
 
-class SerieDetails : AppCompatActivity() {
+class SerieDetails : AppCompatActivity(), SeasonFragment.OnFragmentInteractionListener,
+    AssociatedSeriesFragment.OnFragmentInteractionListener, CommentsFragment.OnFragmentInteractionListener{
+
+    private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_serie_details)
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
+
+        // Set up the ViewPager with the sections adapter.
+        serie_view_pager.adapter = mSectionsPagerAdapter
+        serie_view_pager.currentItem = 0
+
+        serie_view_pager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(serie_tabs))
+        serie_tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(serie_view_pager))
+    }
+
+    /**
+     * A [FragmentPagerAdapter] that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        override fun getItem(position: Int): Fragment {
+            return when (position){
+                0 -> SeasonFragment()
+                1 -> AssociatedSeriesFragment()
+                2 -> CommentsFragment()
+                else -> Fragment()
+            }
+        }
+
+        override fun getCount(): Int {
+            // Show 5 total pages.
+            return 3
+        }
+
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
