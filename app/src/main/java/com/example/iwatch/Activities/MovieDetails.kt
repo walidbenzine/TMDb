5012ecd0922a0ad8b3c9f5efda67e3de980dfc7b
amelazzi <ea_azzi@esi.dz>
2020-04-02 13:19:18 +0200

    feat: add movie detail V1

---
+++ app/src/main/java/com/example/iwatch/Activities/MovieDetails.kt
@@ -0,0 +1,15 @@
+package com.example.iwatch.Activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.iwatch.R
+import com.github.ivbaranov.mfb.MaterialFavoriteButton
+
+
+class MovieDetails : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
