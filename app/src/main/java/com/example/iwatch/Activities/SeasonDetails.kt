df2bc74e1b0145bec066bcaff05df649ff1a38e5
amelazzi <ea_azzi@esi.dz>
2020-04-05 16:33:51 +0200

    feat: season details activity layout

---
+++ app/src/main/java/com/example/iwatch/Activities/SeasonDetails.kt
@@ -0,0 +1,13 @@
+package com.example.iwatch.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.iwatch.R
+
+class SeasonDetails : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_season_details)
+    }
+}
