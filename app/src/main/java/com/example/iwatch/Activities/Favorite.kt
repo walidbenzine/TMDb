c6a4915d27dab5e185824e0c7d415729f4f1e2c4
amelazzi <ea_azzi@esi.dz>
2020-04-08 16:18:16 +0200

    feat: add favortie activity

---
+++ app/src/main/java/com/example/iwatch/Activities/Favorite.kt
@@ -0,0 +1,13 @@
+package com.example.iwatch.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.iwatch.R
+
+class Favorite : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+    }
+}
