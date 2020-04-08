5fc642192605674bc9238240ae243dc281629598
amelazzi <ea_azzi@esi.dz>
2020-04-08 16:49:51 +0200

    feat: attribute fragments to Favorite tabs

--- app/src/main/java/com/example/iwatch/Activities/Favorite.kt
+++ app/src/main/java/com/example/iwatch/Activities/Favorite.kt
@@ -1,13 +1,61 @@
 package com.example.iwatch.Activities
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.iwatch.Fragments.FavoriteMovieFragment
+import com.example.iwatch.Fragments.FavoriteRoomFragment
 import com.example.iwatch.R
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_favorite.*
 
-class Favorite : AppCompatActivity() {
+class Favorite : AppCompatActivity(), FavoriteMovieFragment.OnFragmentInteractionListener,
+    FavoriteRoomFragment.OnFragmentInteractionListener{
+
+    private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorite)
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
+
+        // Set up the ViewPager with the sections adapter.
+        favorite_view_pager.adapter = mSectionsPagerAdapter
+        favorite_view_pager.currentItem = 0
+
+        favorite_view_pager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(favorite_tabs))
+        favorite_tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(favorite_view_pager))
+    }
+
+    /**
+     * A [FragmentPagerAdapter] that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    var name : String = "amel"
+    class SectionsPagerAdapter(fm: FragmentManager)  : FragmentPagerAdapter(fm) {
+
+        override fun getItem(position: Int): Fragment {
+            return when (position){
+                0 -> FavoriteMovieFragment()
+                1 -> FavoriteRoomFragment()
+                else -> Fragment()
+            }
+        }
+
+        override fun getCount(): Int {
+            // Show 2 total pages.
+            return 2
+        }
+
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
