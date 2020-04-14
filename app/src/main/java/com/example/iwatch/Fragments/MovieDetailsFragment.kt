244d378f62a9e0a136530c78ecc3c66a5a4572e2
amelazzi <ea_azzi@esi.dz>
2020-04-15 00:05:00 +0200

    feat: movie actors list

--- app/src/main/java/com/example/iwatch/Fragments/MovieDetailsFragment.kt
+++ app/src/main/java/com/example/iwatch/Fragments/MovieDetailsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.iwatch.Fragments
 
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -9,9 +10,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.iwatch.Activities.PersonDetails
 import com.example.iwatch.Activities.user
 import com.example.iwatch.Adapters.ActorAdapter
 import com.example.iwatch.Adapters.AssociatedFilmAdapter
+import com.example.iwatch.Adapters.OnActorClickListener
 import com.example.iwatch.Entities.Actor
 import com.example.iwatch.Entities.Comment
 import com.example.iwatch.Entities.Movie
@@ -27,7 +30,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [MovieDetailsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MovieDetailsFragment : Fragment() {
+class MovieDetailsFragment : Fragment(), OnActorClickListener {
     // TODO: Rename and change types of parameters
 
     private var listener: OnFragmentInteractionListener? = null
@@ -57,7 +60,7 @@ class MovieDetailsFragment : Fragment() {
 
         actorRecyclerView.apply {
             layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.HORIZONTAL, false)
-            adapter = ActorAdapter(actors)
+            adapter = ActorAdapter(actors, this@MovieDetailsFragment)
         }
 
         val associatedFilmRecyclerView = v.findViewById<RecyclerView>(R.id.movie_detail_associated_films)
@@ -123,5 +126,11 @@ class MovieDetailsFragment : Fragment() {
                     putSerializable(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onActorClicked(actor: Actor) {
+        val actorDetailsIntent = Intent(this.context, PersonDetails::class.java)
+        actorDetailsIntent.putExtra("actor", actor)
+        startActivity(actorDetailsIntent)
     }
 }
