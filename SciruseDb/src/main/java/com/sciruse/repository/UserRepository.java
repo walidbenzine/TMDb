a2ba70a288e57138783e30c0967edcb82d078f5b
islam bourouina <islambourouina@gmail.com>
2020-04-14 16:22:11 +0200

    password change api

--- SciruseDb/src/main/java/com/sciruse/repository/UserRepository.java
+++ SciruseDb/src/main/java/com/sciruse/repository/UserRepository.java
@@ -45,4 +45,9 @@ public interface UserRepository extends CrudRepository<User, Long>{
 	@Transactional
 	void addUserHistFilm(Integer user_id, Integer filmhistory);
 	
+	@Modifying
+	@Query(value = "UPDATE `user` SET `password`=?2 WHERE `id`=?1;", nativeQuery = true)
+	@Transactional
+	void changepass(Integer id,String password);
+	
 }
